eadc7d58940a6d5a84832782a1a84d5db6af1d8a
IamGagan404 <gagansd10@gmail.com>
2025-09-11 23:45:29 +0530

    foo

--- DSA/LC Patterns/03_Sliding Window.py
+++ DSA/LC Patterns/03_Sliding Window.py
@@ -13,3 +13,39 @@ def findMaxAverage(nums,k):
 # 3. Longest Substring without Repeating Characters
 
 
+
+
+# 76. Minimum Window Substring
+def min_win(s,t):
+    if len(t) > len(s):
+        return ""
+    elif len(t) == len(s):
+        return s if sorted(s) == sorted(t) else ""
+    else:
+        window,t_dict = {},{}
+        for c in t:
+            t_dict[c] = 1 + t_dict.get(c,0)
+        have,need = 0, len(t_dict)
+        res,resLen = [-1,-1], float("inf")
+        l = 0
+
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c,0)
+            if c in t_dict and t_dict[c] == window[c]:
+                have += 1
+
+            while have == need:
+                if (r-l+1) < resLen:
+                    res = [l,r]
+                    resLen = r-l+1
+                window[s[l]] -= 1
+                if s[l] in t_dict and t_dict[s[l]] > window[s[l]]:
+                    have -= 1
+                l += 1
+        l,r = res
+        return s[l:r+1] if resLen != float("inf") else ""
+print(min_win(s = "ADOBECODEBANC", t = "ABC"))
+
+
+
--- DSA/Searching.py
+++ DSA/Searching.py
@@ -1,7 +1,6 @@
 def binary_search_iterative(nums, k):  # O(logN) O(1)
     left = 0
     right = len(nums) - 1
-    mid = 0
 
     while left <= right:
         mid = (left + right) // 2
@@ -12,6 +11,7 @@ def binary_search_iterative(nums, k):  # O(logN) O(1)
         else:
             return mid
     return -1
+# print(binary_search_iterative([1,2,3,3,3,4,5,5,5,6,9,10],5))
 
 
 def binary_search_recursive(left, right, k, nums):  # O(logN) O(logN)
@@ -25,9 +25,10 @@ def binary_search_recursive(left, right, k, nums):  # O(logN) O(logN)
             return binary_search_recursive(mid + 1, right, k, nums)
     else:
         return -1
+# print(binary_search_recursive(0,10,5,[1,2,3,3,3,4,5,5,5,6,9,10]))
 
 
-def binary_search(nums,target,findfirst):
+def binary_search_first(nums,target,findfirst):
     start,end = 0,len(nums)-1
     ans = -1
     while start <= end:
@@ -43,5 +44,173 @@ def binary_search(nums,target,findfirst):
             else:
                 start = mid + 1
     return ans
+# print(binary_search_first([1,2,3,3,3,4,5,5,5,6,9,10],5,False))
 
-print(binary_search([1,2,3,3,3,4,5,5,5,6,9,10],5,False))
+def binary_search_lower_bound(nums,k):
+    # find the smallest index lesser or equal to k arr[ind] >= k
+    l,h = 0, len(nums)-1
+    ans = len(nums)
+
+    while l <= h:
+        mid = (l+h) // 2
+        if nums[mid] >= k:
+            ans = mid
+            h = mid - 1
+        else:
+            l = mid + 1
+    return ans
+# print(binary_search_lower_bound([1,2,3,5,6,9,12,45],3))
+
+def binary_search_upper_bound(nums,k):
+    # find the smallest index greater than k arr[ind] > k
+    l,h = 0, len(nums)-1
+    ans = len(nums)
+
+    while l <= h:
+        mid = (l+h)//2
+        if nums[mid] > k:
+            ans = mid
+            h = mid - 1
+        else:
+            l = mid + 1
+    return ans
+# print(binary_search_upper_bound([1,2,3,5,6,9,12,45],3))
+
+# find first and last occurrence of the element in array
+def first_last_occ(nums,k):
+    # first occ => lower bound, last occ => upper bound
+    def lower_bound(nums,k):
+        l,h = 0,len(nums)-1
+        ans = len(nums)
+
+        while l <= h:
+            mid = (l+h)//2
+            if nums[mid] >= k:
+                ans = mid
+                h = mid - 1
+            else:
+                l = mid + 1
+        return ans
+    def upper_bound(nums,k):
+        l,h = 0, len(nums)-1
+        ans = len(nums)
+
+        while l<=h:
+            mid = (l+h)//2
+            if nums[mid] > k:
+                ans = mid
+                h = mid - 1
+            else:
+                l = mid + 1
+        return ans
+
+    lb = lower_bound(nums,k)
+    ub = upper_bound(nums,k)
+    if lb == len(nums) or nums[lb] != k:
+        return [-1,-1]
+    else:
+        return [lb,ub-1]
+# print(first_last_occ([1,2,3,4,4,4,4,5,6,6,6,7],6))
+
+
+def first_occ(nums,k):
+    l,h = 0,len(nums)-1
+    first = -1
+    while l<=h:
+        mid = (l+h)//2
+        if nums[mid] == k:
+            first = mid
+            h = mid - 1
+        elif nums[mid] > k:
+            h = mid - 1
+        else:
+            l = mid + 1
+    return first
+# print(first_occ([2,8,8,8,8,8,11,13],8))
+
+def last_occ(nums,k):
+    l,h = 0,len(nums)-1
+    last = -1
+    while l<=h:
+        mid = (l+h)//2
+        if nums[mid] == k:
+            last = mid
+            l = mid + 1
+        elif nums[mid] < k:
+            l = mid + 1
+        else:
+            h = mid - 1
+    return last
+# print(last_occ([2,8,8,8,8,8,11,13],8))
+
+# search in rotated array with unique elements
+def search_rotated_array_unique(nums,k):
+    # identify sorted half then if it lies in sorted half take that half else other half is sorted
+    l,h = 0, len(nums)-1
+
+    while l<=h:
+        mid = (l+h)//2
+        if nums[mid] == k:
+            return mid
+        if nums[l] <= nums[mid]: # left half is sorted
+            if nums[l] <= k <= nums[mid]:
+                h = mid - 1
+            else:
+                l = mid + 1
+        else:                   # right half is sorted
+            if nums[mid] <= k <= nums[h]:
+                l = mid + 1
+            else:
+                h = mid - 1
+    return -1
+# print(search_rotated_array_unique([7,8,9,1,2,3,4,5,6],95))
+
+# # search in rotated array with unique elements
+def search_rotated_array_dup(nums,k):
+    l,h = 0, len(nums)-1
+
+    while l <= h:
+        mid = (l+h)//2
+        if nums[mid] == k:
+            return True
+        if nums[l] == nums[mid] == nums[h]: # one edge case different from unique, shrink down search space
+            l += 1                          # from both sides in case 3 points are same, we cant get which
+            h -= 1                          # half is sorted
+            continue
+        if nums[l] <= nums[mid]:
+            if nums[l] <= k <= nums[mid]:
+                h = mid - 1
+            else:
+                l = mid + 1
+        else:
+            if nums[mid] <= k <= nums[h]:
+                l = mid + 1
+            else:
+                h = mid - 1
+    return False
+# print(search_rotated_array_unique()[3,3,3,3,3,3,3,3,3,3],3)
+
+# find minimum in rotated sorted array
+def min_rotate(nums):
+    l,h = 0, len(nums)-1
+    ans = float("inf")
+    while l <= h:
+        mid  = (l+h)//2
+        if nums[l] <= nums[h]:  # optimization when both parts are sorted
+            return nums[l]
+        if nums[l] == nums[mid] == nums[h]: # for duplicates
+            l += 1
+            h -= 1
+            continue
+        if nums[l] <= nums[mid]:
+            ans = min(ans,nums[l])
+            l = mid + 1
+        else:
+            ans = min(ans,nums[mid])
+            h = mid - 1
+    return ans
+print(min_rotate([1,1,1,1,1,1,1,1,1,2,3]))
+
+
+
+
