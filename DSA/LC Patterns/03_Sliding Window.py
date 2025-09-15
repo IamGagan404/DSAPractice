61165bd381e72a65595a52f7880604dcc567cfc8
GaganMac <gagansd10@gmail.com>
2025-09-15 23:36:57 +0530

    sw

--- DSA/LC Patterns/03_Sliding Window.py
+++ DSA/LC Patterns/03_Sliding Window.py
@@ -45,7 +45,80 @@ def min_win(s,t):
                 l += 1
         l,r = res
         return s[l:r+1] if resLen != float("inf") else ""
-print(min_win(s = "ADOBECODEBANC", t = "ABC"))
+# print(min_win(s = "ADOBECODEBANC", t = "ABC"))
 
+
+
+# longest substrign without repeating charater
+def long_subs(s):
+    seen = [-1] * 256
+    l,r = 0,0
+    maxl = 0
+    m,n = 0,0
+    while r < len(s):
+        if seen[ord(s[r])] != -1:
+            if seen[ord(s[r])] >= l:
+                l = seen[ord(s[r])] + 1
+        cur_len = r-l+1
+        maxl = max(maxl,cur_len)
+        seen[ord(s[r])] = r
+        r += 1
+    return maxl,s[l:r+1]
+# print(long_subs("cadbzabcd"))
+
+
+# max con ones 3
+def max_con_3(nums,k): # O(2N)
+    l,r = 0,0
+    z = 0
+    maxl = 0
+
+    while r < len(nums):
+        if nums[r] == 0:
+            if z == k:
+                while nums[l] == 1:
+                    l += 1
+                l += 1
+            else:
+                z += 1
+        maxl = max(maxl,r-l+1)
+        r += 1
+    return maxl
+# print(max_con_3(nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3) )
+
+def max_cons_3_optimal(nums,k): # O(N)
+    l,r,z,maxl = 0,0,0,0
+    while r < len(nums):
+        if nums[r] == 0:
+            z += 1
+        if z > k:
+            if nums[l] == 0:
+                z -= 1
+            l += 1
+        if z <= k:
+            maxl = max(maxl,r-l+1)
+        r += 1
+    return maxl
+# print(max_cons_3_optimal(nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3) )
+
+
+# fruits into basket
+def fruits_basket(fruits,k):
+    fruits = list(fruits)
+    l,r = 0,0
+    maxl = 0
+    fmap = {}
+    while r < len(fruits):
+        fmap[fruits[r]] = 1 + fmap.get(fruits[r],0)
+        if len(fmap.keys()) > k:
+            fmap[fruits[l]] -= 1
+            if fmap[fruits[l]] == 0:
+                del fmap[fruits[l]]
+            l += 1
+        if len(fmap.keys()) <= k:
+            maxl = max(maxl,r-l+1)
+        r += 1
+    return maxl
+# print(fruits_basket(fruits= "abcddefg" , k = 3))
 
 
